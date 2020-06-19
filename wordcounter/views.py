1feb3c243a6222363f3da4d270f68f2f1cab5c6c
Sufee <ksufee3@outlook.com>
2020-06-19 07:17:10 +0530

    This is my first commit in GIT

---
+++ wordcounter/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def home(request):
+    return HttpResponse('home')
+
+def project(request):
+    return render(request,'project.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    #print(fulltext)
+
+    wordlist = fulltext.split()
+
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #increase
+            worddictionary[word] +=1
+        else:
+            #add to the worddictionary
+            worddictionary[word] = 1
+
+    sortedwords = sorted(worddictionary.items(),key=operator.itemgetter(1),reverse=True)
+
+
+    return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist),'sortedwords':sortedwords})
+
+
+def about(request):
+    return render(request,'about.html')
